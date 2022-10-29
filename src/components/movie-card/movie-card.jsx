09f43c4ae7f18bc102902a5bdaf8120cfa687168
coleaiseosa <coleaiseosa@gmail.com>
2022-10-29 07:09:08 -0700

    defined proptypes for all components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -25,6 +25,11 @@ MovieCard.propTypes = {
     ImagePath: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
     }),
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
